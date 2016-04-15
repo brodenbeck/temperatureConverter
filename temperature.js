007c3e0e0427a67fd637dae8f22957e6b3ebc4b6
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2016-04-15 15:44:54 -0400

    initial commit

---
+++ temperature.js
@@ -0,0 +1,19 @@
+var fahrenheit = 0;
+
+var celsius = 0;
+
+function toCel(f) {
+	celsius = (5 / 9) * (f - 32);
+}
+
+toCel(fahrenheit);
+
+console.log(fahrenheit + " degrees Fahrenheit converts to " + celsius + " degrees Celsius.")
+
+function toFahr(c) {
+	fahrenheit = (9 / 5) * c + 32;
+}
+
+toFahr(celsius);
+
+console.log(celsius + " degrees Celsius converts to " + fahrenheit + " degrees Fahrenheit.")
