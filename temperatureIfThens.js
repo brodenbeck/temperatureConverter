ce780130845891994effbfc56f7047056ff89287
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2016-04-17 16:20:19 -0400

    added string explanations

--- temperatureIfThens.js
+++ temperatureIfThens.js
@@ -4,9 +4,11 @@ var convertedTemp;
 
 if(unit === "F") {
 	convertedTemp = (temp - 32) * 5 / 9;
+	convertedTemp = temp + " degrees Fahrenheit converts to " + convertedTemp + " degrees Celsius."
 }
 else {
 	convertedTemp = (temp * 9/5 + 32);
+	convertedTemp = temp + " degrees Celsius converts to " + convertedTemp + " degrees Fahrenheit."
 }
 
 console.log(convertedTemp);
