26be4f03fc476efd5cda64346c1b6202111c07aa
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2016-04-18 07:43:41 -0400

    added an additional condition

--- temperatureIfThens.js
+++ temperatureIfThens.js
@@ -6,9 +6,12 @@ if(unit === "F") {
 	convertedTemp = (temp - 32) * 5 / 9;
 	convertedTemp = temp + " degrees Fahrenheit converts to " + convertedTemp + " degrees Celsius."
 }
-else {
+else if (unit === "C") {
 	convertedTemp = (temp * 9/5 + 32);
 	convertedTemp = temp + " degrees Celsius converts to " + convertedTemp + " degrees Fahrenheit."
+}
+else {
+	convertedTemp = 'Please indicate whether your temperature is in Fahrenheit ("F") or in Celsius ("C").'
 }
 
 console.log(convertedTemp);
